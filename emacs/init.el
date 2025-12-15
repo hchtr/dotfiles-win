6759469b84c30fca6b454efd631164dfb4171add
hchtr <155396759+hchtr@users.noreply.github.com>
2025-12-15 22:28:58 +0100

    Create init.el

---
+++ emacs/init.el
@@ -0,0 +1,16 @@
+(setq inhibit-startup-screen t)
+(setq make-backup-files nil)
+(setq default-directory (concat (getenv "HCHTR_HOME") "/"))
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+(delete-selection-mode 1)
+(global-display-line-numbers-mode 1)
+(electric-pair-mode 1)
+
+(set-face-attribute 'default nil :height 165)
+(set-frame-font "Consolas")
+
+(add-to-list 'custom-theme-load-path
+             (concat (getenv "HCHTR_HOME") "/repos/dotfiles-win/emacs/themes"))
+(load-theme 'hchtr t)
